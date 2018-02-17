6f7395baaa8469149248e694086d0159b33ce9fe
Minor G <minorg@server.fake>
2018-02-17 14:27:43 -0600

    hbs support

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
@@ -5,9 +7,37 @@ module.exports = {
   entry: {
     app: ['./js/entry.js']
   },
-  output: {
-    filename: './js/output.js'
+  module: {
+    loaders: [
+      {
+        test: /\.hbs$/,
+        loader: 'handlebars-loader'
+      }
+    ]
   },
+  output: {
+    filename: './js/output.js',
+    path: path.join(__dirname, 'dist')
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new CopyWebpackPlugin([
+      {
+        from: "src/css/",
+        to: "css/"
+      }, {
+        from: "src/fonts/",
+        to: "fonts/"
+      }, {
+        from: "src/img/",
+        to: "img/"
+      }
+    ]),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'src/index.hbs'
+    })
+  ],
   resolveLoader: {
     modules: [path.join(__dirname, 'node_modules')]
   }
